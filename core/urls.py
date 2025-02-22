ba7f6acf33d2b6ce8e71bc2788d7d6cee365390f
saltyypanda <esh7943@rit.edu>
2025-02-22 16:12:39 -0500

    Revamp navbar

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import test_view
+from .views import test_view, caregiver_view, patient_view
 
 urlpatterns = [
     path("", test_view.home, name="home"),
+    path("patient/", test_view.home, name="patient-dashboard"),
+    path("caretaker/", test_view.home, name="caretaker-dashboard"),
 ]
