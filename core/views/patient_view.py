853c037d955425d55ca5baa4234dc10d0daf5c9e
saltyypanda <esh7943@rit.edu>
2025-02-22 17:09:30 -0500

    pushing tehe

--- core/views/patient_view.py
+++ core/views/patient_view.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from ..models import Node
 
-def home(request):
-    return render(request, "home.html")
+def render_nodes(request):
+    # categories = Node.objects.all()
+    categories = ['Category 1', 'Category 2', 'Category 3']
+    return render(request, "nodes.html", {"nodes": categories})
