70e0110024d15113a86f182216cb257791e836fb
Anty120 <anthonyhug222@gmail.com>
2025-02-22 17:57:59 -0500

    added admin view and uploaded file example

--- core/models.py
+++ core/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Node(models.Model):
     name = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    image = models.ImageField(upload_to='uploads/', null=True, blank=True)
     parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='children')
     
     def __str__(self):
-        return f"Node '{self.name}' has {self.children.count()} children"
+        return self.name
     
     def print_children(self):
         children = self.children.all()
--- instructions.md
+++ instructions.md
@@ -4,6 +4,8 @@ In one, run `pip install -r requirements.txt` (if you haven't done so)
 
 Then run `py manage.py tailwind install` (if you haven't done so)
 
+Then cd into your theme/static_src directory, and run `npm i`, to make sure you install all the npm dependencies
+
 Then run `py manage.py tailwind start`
 
-In two, run `py manage.py runserver` and open the URI provided
+In two, run `py manage.py runserver` and open the URL provided
