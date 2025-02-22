a116007d33af8c4e613af479a17bf73a6e656e74
saltyypanda <esh7943@rit.edu>
2025-02-22 18:44:57 -0500

    Pushing to debug

--- core/views/patient_view.py
+++ core/views/patient_view.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from ..models import Node
 from ..DTOs.NodeDTO import load_nodes_from_json
 
-def render_nodes(request):
+def render_nodes(request, node_id=None):
     # categories = Node.objects.all()
     nodes = load_nodes_from_json()
     return render(request, "nodes.html", {"nodes": nodes})
+
+def print_something(request, node_id):
+    print("OSAHFDLASDF " + node_id)
+    return HttpResponse("Hello, world. You're at the polls index.")
