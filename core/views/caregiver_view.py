5a9acbfaaaf991362845663a9ee6f5ecd7681c95
Max Liu <maxliu@Maxs-MacBook-Air-3.local>
2025-02-22 23:08:23 -0500

    commit

--- core/views/caregiver_view.py
+++ core/views/caregiver_view.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render, HttpResponse
 from django.http import HttpResponseRedirect
 from ..models import Node
+catagories = ["Choose your Catagories"]
 
 def home(request):
-    objects = ["Choose your Object","Apple","Pinnaple","Blueberry","Tables"]
-    return render(request, "caretaker/caretakerHome.html",{"objects":objects})
+    return render(request, "caretaker/caretakerHome.html",{"objects":catagories})
 
 def upload(request):
     if request.method == "POST":
         print(request.POST)
         uploaded_file = request.FILES['myFile']
-        Node.objects.create(name="Random",image = uploaded_file)
+        uploaded_name = request.POST.get('myName')
+        catagories.append(request.POST.get('newObject'))
+        Node.objects.create(name= uploaded_name,image = uploaded_file)
     return HttpResponse("good")
+
+def newCat(request):
+    if request.method == "POST":
+        print(request.POST)
+        catagories.append(request.POST.get('newObject'))
+    return render(request,"caretaker/caretakerHome.html",{"objects":catagories})
