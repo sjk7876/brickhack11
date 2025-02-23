f3a8e0c032feaf294017bd73cbbca5d620849183
Spencer Kurtz <sjk7876@rit.edu>
2025-02-23 05:27:48 -0500

    fix ouput categorize

--- core/openaishit/CategorizeWords.py
+++ core/openaishit/CategorizeWords.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from dotenv import load_dotenv
 from openai import OpenAI
@@ -10,10 +11,7 @@ to use, import catoregizeWordList from CategorizeWords.py
 
 catoregizeWordList(words):
     words - a string of words seperated by commas
-    returns - a string of words and their categories in the format Word: {'word'} Category: {'category'}, Word: {'word'} Category: {'category'}
-    
-(i can change the format to something better if you want)
-
+    returns - json node object    
 '''
 
 def getClient():
@@ -24,25 +22,30 @@ def getClient():
 def getContext():
     context = "You will be given a list of words seperated by commas. "
     context += "For example - chicken, water, beer, bread. "
-    context += "Create a few subcategories for the list of words. "
-    context += "Try to minimize the number of subcategories. "
-    context += "Assign each word to one subcategory. "
+    context += "Create a few categories for the list of words. "
+    context += "Try to minimize the number of categories. "
+    context += "Assign each word to one category. "
     context += "For example, chicken -> animal, water -> drink, beer -> drink, bread -> food. "
     context += "Each category should have at least one word. "
-    context += "Return the subcategories for each word in a comma seperated list. "
-    context += "Follow the format - Word: {'word'} Category: {'category'}, Word: {'word'} Category: {'category'}. "
+    context += "You can create relevant subcategories of categories. "
+    context += "For example, lettuce -> vegetable, spinach -> vegetable, vegetable -> food, chicken -> meat, meat -> food. "
+    context += "Minimize the number of subcategories. "
+    # context += "If there is only one word in a category, do not create a subcategory. "
+    # context += "If there is only one subcategory in a category, do not create a subcategory. "
+    context += "Do not return a subcategory if there is only one word in a category. "
+    context += "Return the categories for each word and the subcategories of each category in a JSON format "
+    context += "when returning, do not separate the words and subcategories. "
+    context += "Follow the format - {'categories': [{'word': 'chicken', 'category': 'meat'}, {'word': 'meat', 'category': 'food'}, {'word': 'water', 'category': 'drink'}]}. "
     
     return context
 
 def getPrompt(words):
     s = ""
     for w in words.split(","):
-        s += w.rstrip() + ", "
+        s += w.lstrip() + ", "
     return s
 
 def callOpenAI(context, prompt, client):
-    context = getContext()
-    
     response = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
@@ -50,9 +53,46 @@ def callOpenAI(context, prompt, client):
             {"role": "user", "content": prompt}
         ],
         temperature=0.7,
+        response_format={"type": "json_object"}
     )
     
     return response.choices[0].message.content
+
+
+
+def strToNode(s):
+    def get_or_create_category(category):
+        if category not in category_map:
+            category_map[category] = {
+                "id": str(len(category_map) + 1),
+                "name": category.capitalize(),
+                "image": f"/static/images/{category}.png",
+                "children": []
+            }
+        return category_map[category]
+
+    
+    
+    data = json.loads(s)
+    
+    category_map = {}
+
+    for entry in data["categories"]:
+        word = entry["word"]
+        category = entry["category"]
+        category_node = get_or_create_category(category)
+        word_node = get_or_create_category(word)
+        
+        # Avoid duplicate insertions
+        if word_node not in category_node["children"]:
+            category_node["children"].append(word_node)
+    
+    top_level_categories = [cat for cat in category_map.values() if all(cat not in v["children"] for v in category_map.values())]
+
+    print(json.dumps(top_level_categories, indent=2))
+
+    
+    return top_level_categories
 
 def catoregizeWordList(words):
     client = getClient()
@@ -60,28 +100,24 @@ def catoregizeWordList(words):
     context = getContext()
     prompt = getPrompt(words)
     
-    phrase = callOpenAI(context, prompt, client)
+    categories = callOpenAI(context, prompt, client)
     
-    return phrase
+    print(categories)
+    print("\n")
+    
+    node = strToNode(categories)
+    
+    return node
 
 def main():
     # wordlist = input("words: ")
     wordlist = "chicken, water, carrot, wallet, bottle, tree, hat, shirt, spinach, cow, steak, bush, watch, lemonade, beer, bread, book"
     
-    categories = catoregizeWordList(wordlist)
+    node = catoregizeWordList(wordlist)
     
-    for c in categories.split(", "):
-        print(c)
-    # print(categories)
-    
-    # context = getContext()
-    # prompt = getPrompt(wordlist)
-    
-    # phrase = callOpenAI(context, prompt, getClient())
-    
-    # print(phrase)
-    
-    # generateImage(phrase)
+    print("==========================================")
+    print(type(node))
+    print(node)
 
 if __name__ == "__main__":
     main()
