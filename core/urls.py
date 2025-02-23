f2c8fdbc90cf5eb80666c52f2a55a49dcb5e8e88
Anty120 <anthonyhug222@gmail.com>
2025-02-22 21:49:36 -0500

    added auth0 functionality

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import test_view, caregiver_view, patient_view
+from .views import test_view, caregiver_view, patient_view, auth_view
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", test_view.home, name="home"),
+    path("home/", auth_view.index, name="index"),
+    path("", auth_view.login, name="login"),
+    path("logout", auth_view.logout, name="logout"),
+    path("callback", auth_view.callback, name="callback"),
     path("patient/", patient_view.render_nodes, name="patient-dashboard"),
     path("caretaker/", caregiver_view.home, name="caretaker-dashboard"),
 ]
