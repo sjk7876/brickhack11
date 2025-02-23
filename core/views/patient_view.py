67c2fea03532fede856f9708a9f5babf5c4ac835
saltyypanda <esh7943@rit.edu>
2025-02-22 19:26:21 -0500

    Completed traversing of children from db

--- core/views/patient_view.py
+++ core/views/patient_view.py
@@ -4,11 +4,11 @@ from ..DTOs.NodeDTO import load_nodes_from_json
 
 NODES = load_nodes_from_json()
 
-def render_nodes(request):
-    categories = Node.objects.all()
-    return render(request, "nodes.html", {"nodes": categories})
-
-def render_children(request, node_id):
-    node = Node.objects.get(id=node_id)
-    children = node.children.all()
-    return render(request, "nodes.html", {"nodes": children})
+def render_nodes(request, node_id=None):
+    if node_id is None:
+        nodes = Node.objects.filter(parent=None)
+    else:
+        parent = Node.objects.get(id=node_id)
+        nodes = parent.children.all()
+        
+    return render(request, "nodes.html", {"nodes": nodes})
